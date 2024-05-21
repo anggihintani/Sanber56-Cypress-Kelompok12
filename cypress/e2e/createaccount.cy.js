b6e881ab90ee00fed345ad081ce867b8d00d3f2f
salsabilananda <salsabilanandalmadani@gmail.com>
2024-05-21 13:01:15 +0700

    fixing changes create account

--- cypress/e2e/createaccount.cy.js
+++ cypress/e2e/createaccount.cy.js
@@ -7,8 +7,8 @@ const randomEmail = `test${Math.floor(Math.random() * 100000)}@example.com`;
 
 describe("Navigasi ke halaman create account", () => {
   beforeEach(() => {
-    Navigation.visitHomepage();
     cy.clearAllCookies;
+    cy.visit('https://magento.softwaretestingboard.com/');
     cy.contains("Create an Account").click();
     cy.clearAllCookies;
   });
@@ -31,15 +31,17 @@ describe("Navigasi ke halaman create account", () => {
       cy.get(account.paswd).type(userData.invalidUser1.password);
       cy.get(account.paswdConfirm).type(userData.invalidUser1.passwordconfirmation);
       cy.get(account.submit).click();
+      cy.get('#email_address-error').should('be.visible');
     });
 
-    it('TC-3 : Create account tanpa mengisi last name', function() {
+    it('TC-3 : Create account dengan mengisi password weak/lemah', function() {
       cy.get(account.first).type(userData.invalidUser2.firstName);
       cy.get(account.last).type(userData.invalidUser2.lastName);
       cy.get(account.email).type(randomEmail);
       cy.get(account.paswd).type(userData.invalidUser2.password);
       cy.get(account.paswdConfirm).type(userData.invalidUser2.passwordconfirmation);
       cy.get(account.submit).click();
+      cy.get('#password-strength-meter').should('be.visible');
     });
 
     
